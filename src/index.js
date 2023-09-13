fcccf6234dada29157a1cdb391700f594e804d7b
aemiroo <136221620+oorimea@users.noreply.github.com>
2023-09-13 14:18:16 +0800

    Update index.js
    removed guildId since it's not required when applicationCommands is set on Routes field.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 require('dotenv').config();
 const { Client, GatewayIntentBits, REST, Routes } = require('discord.js');
-const { clientId, guildId } = require('./config');
-
+const { clientId } = require('./config.json');
 const client = new Client({
     intents: [
-        GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent,
-        GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildVoiceStates,
-        GatewayIntentBits.GuildPresences
+        GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildVoiceStates, 
+        GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildPresences
     ],
 });
 
-const rest = new REST().setToken(process.env.TOKEN);
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 
 const prefix = '!';
 require('./loader')(client, prefix);
@@ -21,14 +19,14 @@ require('./loader')(client, prefix);
     try {
         console.log(`Started refreshing ${commands.length} application (/) commands.`);
 
-        for (const guild of guildId.split('|')) {
-            const data = await rest.put (
-                Routes.applicationGuildCommands(clientId, guild),
+        // for (const guild of guildId.split('|')) {
+            const data = await rest.put(
+                Routes.applicationCommands(clientId),
                 { body: commands.map(command => command.data.toJSON()) },
             );
-    
+
             console.log(`Successfully reloaded ${data.length} application (/) commands.`);
-        }
+        // }
     } catch (error) {
         console.error(error);
     }
